395b7cb7d4ef47924574cc0e61ffde62c8c48577
solver-fox <charleshardy1225@gmail.com>
2024-10-29 12:03:25 +0100

    add

--- next.config.js
+++ next.config.js
@@ -7,9 +7,6 @@ const nextConfig = {
         hostname: '**',
       },
     ],
-    domains: ['https://hem-property.vercel.app/'],
-    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
   async headers() {
     return [
